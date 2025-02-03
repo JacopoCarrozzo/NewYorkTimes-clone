996be41fcd93fa1dd3b7876f5f38e6e8acfddb39
Jacopo <jacopocarrozzo19@gmail.com>
2025-02-03 18:25:23 +0100

    Add redirect and fallback route for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom'; 
+import { Routes, Route, Navigate } from 'react-router-dom'; 
 import Login from './components/login.tsx';
 import Main from './components/main.tsx';
 import Details from './components/details.tsx';
 
 const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/NewYorkTimes-clone/" element={<Main />} />
-        <Route path="/NewYorkTimes-clone/login" element={<Login />} />
-        <Route path="/NewYorkTimes-clone/details" element={<Details />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/NewYorkTimes-clone" element={<Navigate to="/NewYorkTimes-clone/" replace />} />
+      <Route path="/NewYorkTimes-clone/" element={<Main />} />
+      <Route path="/NewYorkTimes-clone/login" element={<Login />} />
+      <Route path="/NewYorkTimes-clone/details" element={<Details />} />
+      <Route path="*" element={<Navigate to="/NewYorkTimes-clone/" replace />} />
+    </Routes>
   );
 };
 
