a96e9fe2664c341a7bb2ce5927118c0ac43a52db
Jacopo <jacopocarrozzo19@gmail.com>
2025-02-03 18:09:52 +0100

    Fix double Router issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import { Routes, Route } from 'react-router-dom'; 
 import Login from './components/login.tsx';
 import Main from './components/main.tsx';
 import Details from './components/details.tsx';
 
 const App = () => {
   return (
-    <BrowserRouter basename="/NewYorkTimes-clone">
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/' element={<Main />} />
-        <Route path='/details' element={<Details />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path='/login' element={<Login />} />
+      <Route path='/' element={<Main />} />
+      <Route path='/details' element={<Details />} />
+    </Routes>
   );
 };
 
