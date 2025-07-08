ba3d547f1a38507ec51bb33ea8d043ac8d920fba
JacopoCarrozzo <jacopocarrozzo19@gmail.com>
2025-07-08 10:09:49 +0200

    Update details.jsx

--- src/components/details.jsx
+++ src/components/details.jsx
@@ -2,21 +2,19 @@ import React from 'react';
 import { useLocation, Link } from 'react-router-dom';
 import moment from 'moment';
 import TheNewYorkTimes from "../images/TheNewYorkTimes.png";
-import Footer from './footer'; // Modificato l'import del footer per non avere l'estensione .tsx
+import Footer from './footer';
 import backarrow from '../images/backarrow.png';
 
 const Details = () => {
     const location = useLocation();
-    const article = location.state?.data; // Rimosso l'optional chaining
+    const article = location.state?.data; 
 
     return (
         <>
-            {/* Header con logo */}
             <div className='p-4 shadow-lg flex justify-center items-center'>
                 <img src={TheNewYorkTimes} className='w-40 sm:w-52' alt="NYT Logo" />
             </div>
 
-            {/* Contenuto principale */}
             <div className="w-full sm:w-8/12 mx-auto p-4">
                 <Link to="/">
                     <div className='flex items-center justify-start relative'>
@@ -25,12 +23,10 @@ const Details = () => {
                     </div>
                 </Link>
 
-                {/* Titolo e descrizione */}
                 <div className='sm:w-9/12 mx-auto text-center sm:text-left mt-6'>
-                    <h1 className="font-bold text-2xl">{article ? article.title : "Titolo non disponibile"}</h1>
+                    <h1 className="font-bold text-2xl">{article ? article.title : "Title not available"}</h1>
                 </div>
 
-                {/* Immagine principale */}
                 {article && article.multimedia && article.multimedia[0] && article.multimedia[0].url && (
                     <img
                         className="w-full h-auto mt-4 object-cover rounded-lg"
@@ -39,15 +35,13 @@ const Details = () => {
                     />
                 )}
 
-                {/* Data e Fonte */}
                 <div className='flex flex-col sm:flex-row items-center sm:items-start font-bold mt-4 text-gray-600'>
                     <h1>{moment(article?.pub_date).format('DD-MM-YYYY')}</h1>
                     {article && article.source && <h1 className='sm:ml-3'>• {article.source}</h1>}
                 </div>
 
-                {/* Contenuto dell'articolo */}
                 <p className="font-serif text-lg mt-3 sm:w-9/12 mx-auto text-justify">
-                     {article?.snippet || article?.abstract || "Descrizione non disponibile."}
+                     {article?.snippet || article?.abstract || "Description not available."}
                 </p>
             </div>
 
