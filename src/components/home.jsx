66b5cc8e0afac5b0aeda0ddb10b48b116194fe4d
JacopoCarrozzo <jacopocarrozzo19@gmail.com>
2025-07-08 10:12:59 +0200

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -6,15 +6,15 @@ const Home = ({ news, num }) => {
     return (
         <div className="w-full sm:w-8/12 mx-auto p-4">
             {news.length === 0 ? (
-                <p>Caricamento in corso...</p>
-            ) : news.length > num && news[num] ? (  // ✅ Controllo aggiuntivo per evitare errori di array fuori limite
+                <p>Loading in progress...</p>
+            ) : news.length > num && news[num] ? (  
                 <Link to="/details" state={{ data: news[num] }} className="block">
                     <div className="flex flex-col sm:flex-row items-center sm:items-start">
-                        {/* Testo */}
+                       
                         <div className="sm:w-1/2 w-full text-center sm:text-left">
                             <h1 className="font-bold text-xl font-serif">{news[num].title}</h1>
                             <h2 className="font-serif text-sm mt-2">{news[num].abstract}</h2>
-                            <Link  // ✅ Cambiato da <span> a <Link> per garantire la navigazione corretta
+                            <Link
                                 to="/details"
                                 state={{ data: news[num] }}
                                 className="text-blue-600 mt-2 inline-block"
@@ -23,7 +23,6 @@ const Home = ({ news, num }) => {
                             </Link>
                         </div>
 
-                        {/* Immagine */}
                         {news[num].multimedia?.[0]?.url && (
                             <img
                                 className="w-full sm:w-8/12 h-auto sm:h-96 mt-4 sm:mt-0 sm:ml-7 object-cover"
@@ -34,7 +33,7 @@ const Home = ({ news, num }) => {
                     </div>
                 </Link>
             ) : (
-                <p>Nessun articolo trovato.</p>
+                <p>No articles found.</p>
             )}
         </div>
     );
